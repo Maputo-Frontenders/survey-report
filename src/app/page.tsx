2ab1bcb426441c86bb7fab8e4f263822b1b805d4
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2023-03-20 19:13:55 +0200

    feat: added the intro section.
    I didn't colorize because we still ain't have the colors.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+import NavBar from '@/components/NavBar'
+
 export default function Home() {
   return (
     <main className='p-5'>
-      <h1 className='text-2xl bg-red-500 text-white p-2 rounded'>Tailwind is working</h1>
+      <NavBar />
+      <section id='intro-section' className='section'>
+        <h1>
+          Intro
+        </h1>
+        <div className='flex flex flex-col md:flex-row gap-12 md:gap-36 justify-between  lg: '>
+          <div className='flex-1'>
+            <p>
+              Lorem ipsum id sit veniam ullamco sed ex ad ut quis qui aliqua labore ad nulla. dolor minim in labore voluptate duis ut voluptate velit aute in veniam magna ut sint magna sed laboris cupidatat labore exercitation
+            </p>
+            <p>
+              Fugiat adipisicing occaecat deserunt tempor qui et deserunt sunt officia tempor voluptate ex cillum laboris consectetur amet eu tempor pariatur ad consequat do sint mollit velit  eu amet tempor veniam sed ullamco duis fugiat consequat dolore ut amet tempor id sint esse reprehenderit aliquip ut do laboris consequat duis sit reprehenderit cupidatat minim cupidatat non nisi exercitation dolor in est laborum anim occaecat ad aliquip officia pariatur dolor sint proident velit nisi duis ut in et laborum nostrud mollit cupidatat id irure cupidatat aliquip aliquip id magna ad reprehenderit adipisicing mollit est nulla pariatur cupidatat veniam voluptate qui esse nisi elit fugiat non sunt adipisicing nisi nisi excepteur cillum qui esse veniam eu in veniam culpa aute laboris dolor eu est do excepteur dolor consequat consectetur nisi consectetur nostrud ullamco consequat dolore commodo aute aliquip adipisicing reprehenderit et enim excepteur occaecat do occaecat anim irure consequat proident ex reprehenderit sit do velit ex ad dolore ex tempor cillum sunt et veniam fugiat in tempor consectetur labore exercitation consequat incididunt exercitation deserunt veniam culpa qui amet amet dolore consectetur adipisicing occaecat in consectetur ea ut fugiat.
+            </p>
+            <p>
+              Fugiat adipisicing occaecat deserunt tempor qui et deserunt sunt officia tempor voluptate ex cillum laboris consectetur amet eu tempor pariatur ad consequat do sint mollit velit dolor minim in labore voluptate duis ut voluptate velit aute in veniam magna ut sint magna sed laboris cupidatat labore exercitation eu amet tempor veniam sed ullamco duis fugiat consequat dolore ut amet tempor id sint esse reprehenderit aliquip ut do laboris consequat duis sit reprehenderit cupidatat minim cupidatat non nisi exercitation dolor in est laborum anim occaecat ad aliquip officia pariatur dolor sint proident velit nisi duis ut in et laborum nostrud eur consequat qui pariatur enim ut id dolore cillum sint eu ex qui consectetur nisi consectetur nostrud ullamco consequat dolore commodo adipisicing anim ut elit  consequat incididunt exercitation deserunt veniam culpa qui amet amet dolore consectetur adipisicing occaecat in consectetur ea ut fugiat.
+            </p>
+          </div>
+          <div className='flex-1'>
+            <p>
+              Fugiat adipisicing occaecat deserunt tempor qui et deserunt sunt officia tempor voluptate ex cillum laboris consectetur amet eu tempor pariatur ad consequat do sint mollit velit dolor minim in labore voluptate duis ut voluptate velit aute in veniam magna ut sint magna sed laboris cupidatat labore exercitation eu amet tempor veniam sed ullamco duis fugiat consequat dolore ut amet tempor id sint esse reprehenderit adipisicing mollit est nulla pariatur cupidatat veniam voluptate qui esse nisi elit fugiat non sunt adipisicing nisi nisi excepteur cillum qui esse veniam eu in veniam culpa aute laboris dolor eu est do excepteur dolor consequat laboris eu officia qui excepteur consequat qui pariatur enim ut id dolore cillum sint eu ex qui consectetur nisi consectetur nostrud ullamco consequat dolore commodo adipisicing anim ut elit deserunt officia id incididunt nostrud in anim reprehenderit commodo aute aliquip adipisicing reprehenderit et enim excepteur occaecat do occaecat anim irure 
+            </p>
+            <p>
+              Tempor irure in sunt sint cupidatat ut laborum ut veniam sunt est do ad laborum deserunt eu dolore ut. consequat proident ex reprehenderit sit do velit ex ad dolore ex tempor cillum sunt et veniam fugiat in tempor consectetur labore exercitation consequat incididunt exercitation deserunt veniam culpa qui amet amet dolore consectetur adipisicing occaecat in consectetur ea ut fugiat.
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
