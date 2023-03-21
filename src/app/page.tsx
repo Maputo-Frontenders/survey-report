313b3c3509a6ed1bc1eda4532338b792323e29da
Elton <eltoncavele8@gmail.com>
2023-03-21 17:06:41 +0200

    feat: add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className='p-5'>
-      <h1 className='text-2xl bg-red-500 text-white p-2 rounded'>Tailwind is working</h1>
+    <main className=''>
+    
     </main>
   )
 }
