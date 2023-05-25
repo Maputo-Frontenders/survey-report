d9ea382948ceef3bd2c393096d29dfd9bb86cb0f
Elton Cavele <ecavele@technoplus.co.mz>
2023-05-25 19:55:41 +0200

    feat(submitForm): adding country api, buy exit error

---
+++ src/app/components/modal/CountryModal.tsx
@@ -0,0 +1,84 @@
+import { Box, Modal } from "@mui/material";
+import React, { useEffect } from "react";
+import Image from "next/image";
+
+interface CountryModalProps {
+  visible: boolean;
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}
+export default function CountryModal({
+  visible,
+  setVisible,
+}: CountryModalProps) {
+  const handleClose = () => {
+    setVisible(false);
+  };
+
+  const [countries, setCountries] = React.useState<any>();
+
+  const style = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 600,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setCountries(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Modal
+        open={visible}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div>
+            <div className="flex items-center gap-10">
+              <span>icon</span>
+              <input
+                className="h-10 w-full p-5 outline-none  border-b-2 border-gray-500"
+                type="text"
+                placeholder="Pesquise o seu pais "
+              />
+            </div>
+            <div>
+              <div>
+                <span>Resultado</span>
+              </div>
+              <div>
+                {countries?.map((country: any, index: number) => [
+                  <div key={index}>
+                    <div>
+                      <Image
+                        src={country.flags.svg}
+                        alt=""
+                        width={20}
+                        height={20}
+                      />
+                    </div>
+                    <div>
+                      <p>{country.name}helloo</p>
+                    </div>
+                  </div>,
+                ])}
+              </div>
+            </div>
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
